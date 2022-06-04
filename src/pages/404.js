5958a147b062d7acfa0a813734852539d6b561f9
Collin Pfeifer <collinpfeifer@Icloud.com>
2022-06-04 17:35:13 -0400

    updated pictures to be smaller

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-const NotFoundPage = () => (
-  <Layout>
-
-  </Layout>
-)
+const NotFoundPage = () => <Layout></Layout>
 
 export default NotFoundPage
